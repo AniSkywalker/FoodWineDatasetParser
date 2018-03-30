353eaad76c8f06d48376ef666f33e4bddaee5f3a
AniSkywalker <aniruddha.ghosh@ucdconnect.ie>
2018-03-30 21:44:57 +0100

    bug fix

--- src/database/DatabaseBrowser.java
+++ src/database/DatabaseBrowser.java
@@ -1,8 +1,5 @@
 package database;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -29,6 +26,7 @@ public class DatabaseBrowser {
     }
 
     public void connect(String username, String password) {
+        // pass your mysql username and password to connect
         try {
             Class.forName("com.mysql.jdbc.Driver");
             connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/?" + "user=" + username + "&password=" + password + "");
@@ -38,6 +36,7 @@ public class DatabaseBrowser {
     }
 
     public void createDatabase(String name) {
+        //creating a database
         try {
             String sql = "create database if not exists " + name + ";";
             if (this.connect != null) {
@@ -53,6 +52,7 @@ public class DatabaseBrowser {
     }
 
     public void useDatabase(String name) {
+        //setting the active database
         try {
             String sql = "use " + name + ";";
             Statement s = this.connect.createStatement();
@@ -63,6 +63,7 @@ public class DatabaseBrowser {
     }
 
     public void executingDDL(String sql) {
+        // executing DDL sql (create, alter)
         try {
             if (this.connect != null) {
                 Statement s = this.connect.createStatement();
@@ -77,6 +78,7 @@ public class DatabaseBrowser {
     }
 
     public void executingDML(String sql) {
+        // executing DML sql
         try {
             Statement st = this.connect.createStatement();
             st.executeUpdate(sql);
